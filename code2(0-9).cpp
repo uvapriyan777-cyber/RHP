0105088407f55c4af79eff6b8bc95a44f3565e6f
uvapriyan777-cyber <uvapriyan777@gmail.com>
2026-06-07 18:22:58 +0530

    Create code2(0-9).cpp

---
+++ code2(0-9).cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    string s;
+    cin >> s;
+
+    int flag = 0;
+
+    for(char ch : s) {
+        if(ch >= '0' && ch <= '9')
+            flag |= (1 << (ch - '0'));
+    }
+
+    cout << (flag == (1 << 10) - 1 ? "Yes" : "No");
+} 
