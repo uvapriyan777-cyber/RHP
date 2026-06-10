99bebacb009a8aed98670d4cfe5e8ffd7d8b331b
uvapriyan777-cyber <uvapriyan777@gmail.com>
2026-06-10 11:44:53 +0530

    Create code6-longest-substring.java

---
+++ code6-longest-substring.java
@@ -0,0 +1,21 @@
+def longest_common_substring(s1, s2):
+    m, n = len(s1), len(s2)
+    dp = [[0] * (n + 1) for _ in range(m + 1)]
+
+    max_len = 0
+    end_pos = 0
+
+    for i in range(1, m + 1):
+        for j in range(1, n + 1):
+            if s1[i - 1] == s2[j - 1]:
+                dp[i][j] = dp[i - 1][j - 1] + 1
+
+                if dp[i][j] > max_len:
+                    max_len = dp[i][j]
+                    end_pos = i
+
+    return s1[end_pos - max_len:end_pos]
+
+# Example
+print(longest_common_substring("ABABC", "BABCA"))
+# Output: BABC
